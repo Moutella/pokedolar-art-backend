fae398524817472018c2a08fabe9a73cb83e9bb3
Moutella <joaogabrielmsc@gmail.com>
2020-06-25 00:28:16 -0300

    remove wrong route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,5 @@
 const Router = require('express');
 const router = new Router();
 
-router.route('/pokemon').get(PokemonController.getPokemons);
-
 router.route('/login/twitter',
   passport.authenticate('twitter'));
