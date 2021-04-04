41da91473e395c6a17097de5ac6b9e18a19e4be9
moutella <joaogabrielmsc@gmail.com>
2021-04-04 13:23:48 -0300

    testFix

--- utils/awsUtils.js
+++ utils/awsUtils.js
@@ -3,23 +3,23 @@ const serverConfig = require("../config")
 const fs = require('fs')
 class AWSUtils{
   constructor(){
+    this.s3 = new awssdk.S3()
+  }
+
+  putFile(file, path){
     awssdk.config.update({
       accessKeyID: serverConfig.AWS_KEY,
       secretAccessKey: serverConfig.AWS_SECRET
       }
     )
-    this.s3 = new awssdk.S3()
-  }
-
-  putFile(file, path){
-    console.log(serverConfig.AWS_KEY);
-    console.log(serverConfig.AWS_SECRET);
     let params = {
       Bucket: serverConfig.AWS_BUCKET,
       Key: path,
       Body: fs.readFileSync(file)
     }
     this.s3.putObject(params, function (err, data) {
+      console.log(data)
+      console.log(err)
       if (err) throw "falha de upload";
     });
   }
